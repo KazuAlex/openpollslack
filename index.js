a94f3f6dc34ca7a7aa1b3739772cade22d2ee461
KazuAlex <kazualex@pm.me>
2019-09-22 16:41:29 +0300

    refactoring button value

--- index.js
+++ index.js
@@ -13,8 +13,39 @@ const port = 5000;
 server.post('/', (req, res) => {
   let question = null;
   const options = [];
+  let body = req.body.text;
+
+  let isAnonymous = false;
+  let isLimited = false;
+  let limit = null;
+  let fetchLimit = false;
+
+  for (let arg of body.substr(0, body.indexOf("\"")).trim().split(' ')) {
+    if (fetchLimit) {
+      limit = parseInt(arg);
+      if (isNaN(limit)) {
+        limit = 1;
+        fetchLimit = false;
+      } else {
+        fetchLimit = false;
+        continue;
+      }
+    }
+
+    if (!isAnonymous && arg === 'anonymous') {
+      isAnonymous = true;
+    } else if (!isLimited && arg === 'limit') {
+      isLimited = true;
+      fetchLimit = true;
+    }
+  }
+
+  if (isLimited && null === limit) {
+    limit = 1;
+  }
+
   if (req.body.text) {
-    for (let option of req.body.text.match(/"[^"\\]*(?:\\[\S\s][^"\\]*)*"|'[^'\\]*(?:\\[\S\s][^'\\]*)*'/g)) {
+    for (let option of body.match(/"[^"\\]*(?:\\[\S\s][^"\\]*)*"|'[^'\\]*(?:\\[\S\s][^'\\]*)*'/g)) {
       let opt = option.substring(1, option.length - 1);
       if (question === null) {
         question = opt;
@@ -36,8 +67,18 @@ server.post('/', (req, res) => {
     type: 'divider',
   });
 
+  let button_value = {
+    anonymous: isAnonymous,
+    limited: isLimited,
+    limit: limit,
+    voters: [],
+    id: null,
+  };
+
   for (let i in options) {
     let option = options[i];
+    btn_value = JSON.parse(JSON.stringify(button_value));
+    btn_value.id = i;
     let block = {
       type: 'section',
       text: {
@@ -51,7 +92,7 @@ server.post('/', (req, res) => {
           emoji: true,
           text: 'Vote',
         },
-        value: 'vote_for_'+i,
+        value: JSON.stringify(btn_value),
       },
     };
     blocks.push(block);
@@ -90,44 +131,40 @@ server.post('/actions', (req, res) => {
     let blocks = message.blocks;
     const actions = payload.actions;
     for (let action of actions) {
-      let value = action.value;
-      value = parseInt(value.substring(9));
-      context_id = 2 + (value * 2) + 1;
+      let value = JSON.parse(action.value);
+      button_id = 2 + (value.id * 2);
+      context_id = 2 + (value.id * 2) + 1;
+      let blockBtn = blocks[button_id];
       let block = blocks[context_id];
-      const voters = block.elements[0].text;
-      let newVoters = null;
-      if (voters === 'No votes') {
-        newVoters = '<@'+ user_id +'> 1 vote';
+      let voters = value.voters ? value.voters : [];
+      let newVoters = '';
+
+      if (voters.includes(user_id)) {
+        voters = voters.filter(voter_id => voter_id != user_id);
       } else {
-        let userFound = false;
-        const allVoters = [];
-        for (let voter of voters.match(/<.*>/g)) {
-          const voter_id = voter.substring(2, voter.length - 1);
-          if (voter_id == user_id) {
-            userFound = true;
-          } else {
-            if (!newVoters) {
-              newVoters = voter;
-            } else {
-              newVoters += ' '+voter;
-            }
-            allVoters.push(voter_id);
-          }
+        voters.push(user_id);
+      }
+
+      if (voters.length === 0) {
+        newVoters = 'No votes';
+      } else {
+        newVoters = '';
+        for (let voter of voters) {
+          newVoters += '<@'+voter+'> ';
         }
 
-        if (!userFound) {
-          allVoters.push(user_id);
-          newVoters += ' <@'+user_id+'>';
-        }
-
-        if (allVoters.length === 1) {
-        } else if (allVoters.length > 1) {
-          newVoters = ' '+ allVoters.length +' votes';
+        newVoters += voters.length +' ';
+        if (voters.length === 1) {
+          newVoters += 'vote';
         } else {
-          newVoters = 'No votes';
+          newVoters += 'votes';
         }
       }
+
+
       block.elements[0].text = newVoters;
+      value.voters = voters;
+      blockBtn.accessory.value = JSON.stringify(value);
       blocks[context_id] = block;
     }
     response.blocks = blocks;
